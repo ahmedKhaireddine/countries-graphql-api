d2b0a5945b4f568dd9d9adbfb4417a7ecc0ab23f
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-06-23 10:50:39 +0200

    Add json attributes of model country

--- src/db/models/Country.js
+++ src/db/models/Country.js
@@ -1,10 +1,27 @@
-import { Model } from "objection";
-import knex from "../knexInstance";
+import { Model } from 'objection';
+import knex from '../knexInstance';
 
 Model.knex(knex);
 
 export default class Country extends Model {
     static get tableName() {
-        return "country";
+        return 'country';
+    }
+
+    static get idColumn() {
+        return 'id';
+    }
+
+    static get jsonAttributes() {
+        return [
+            'area',
+            'borders',
+            'callingCodes',
+            'languages',
+            'latlng',
+            'population',
+            'topLevelDomain',
+            'translations'
+        ];
     }
 }
