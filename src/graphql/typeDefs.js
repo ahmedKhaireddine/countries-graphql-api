25de8a979fb9df1e2d72059c9f4447e92078e22f
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-06-25 09:58:28 +0200

    Create the graphql folder with the necessary files

---
+++ src/graphql/typeDefs.js
@@ -0,0 +1,35 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+    # This "Country" type defines the queryable fields for every country in our data source.
+    type Country {
+        id: Int!
+        alpha2Code: String
+        alpha3Code: String
+        area: String
+        borders: [String]
+        callingCodes: [String]
+        capital: String
+        demonym: String
+        flag: String
+        languages: [String]
+        latlng: [Float]
+        name: String
+        nativeName: String
+        numericCode: String
+        population: String
+        region: String
+        subregion: String
+        topLevelDomain: [String]
+        translations: String
+    }
+
+    # The "Query" type is special: it lists all of the available queries that
+    # clients can execute, along with the return type for each. In this
+    # case, the "countries" query returns an array of zero or more Countries (defined above).
+    type Query {
+        countries: [Country !]
+    }
+`;
--- src/server.js
+++ src/server.js
@@ -1,46 +1,10 @@
 import dotenv from 'dotenv';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs, resolvers } from './graphql/index.js';
 
 dotenv.config();
 
 const { PORT } = process.env;
-
-const typeDefs = gql`
-    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-    # This "Book" type defines the queryable fields for every book in our data source.
-    type Book {
-        id: Int!
-        title: String
-        author: String
-    }
-
-    # The "Query" type is special: it lists all of the available queries that
-    # clients can execute, along with the return type for each. In this
-    # case, the "books" query returns an array of zero or more Books (defined above).
-    type Query {
-        books: [Book !]
-    }
-`;
-
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-];
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-    Query: {
-      books: () => books,
-    },
-};
 
 const Port = PORT || 8080;
 
