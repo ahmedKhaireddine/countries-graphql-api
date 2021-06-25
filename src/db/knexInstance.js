88395b86845a030c666c4292046a6c6c109fed10
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-06-25 09:45:13 +0200

    Improve knex instance

--- src/db/knexInstance.js
+++ src/db/knexInstance.js
@@ -1,27 +1,10 @@
 import knex from "knex";
 import { knexSnakeCaseMappers } from "objection";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const {
-    DB_CONNECTION,
-    DB_HOST,
-    DB_PORT,
-    DB_DATABASE,
-    DB_USERNAME,
-    DB_PASSWORD
-} = process.env;
+import { client, connection } from '../../knexfile.js';
 
 export default knex({
-    client: DB_CONNECTION,
-    connection: {
-        host:  DB_HOST,
-        port:  DB_PORT,
-        user: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: DB_DATABASE,
-    },
+    client,
+    connection,
     ...knexSnakeCaseMappers({
         underscoreBeforeDigits: true,
     }),
