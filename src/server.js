967eee6399f59a2a1ddf53c2a3530f601808240f
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-07-07 15:10:22 +0200

    Improve the server and use static files

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './graphql';
+import express from 'express';
 
 dotenv.config();
 
 const { PORT } = process.env;
-
 const Port = PORT || 8080;
+const app = express();
+
+app.use(express.static('public'));
 
 const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
-server.listen(Port).then(({ url }) => console.log(`Server running ${url}`));
+app.listen(Port, () => {
+    console.log(`🚀  Server ready at http://localhost:4000${ server.graphqlPath }`);
+});
