7cd1360ad3ca4f8d017f38148524d628bd66c8e0
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-07-13 14:54:11 +0200

    Initialize apollo client

---
+++ client/src/graphql/Connect.js
@@ -0,0 +1,11 @@
+import {
+    ApolloClient,
+    InMemoryCache
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql'
+});
+
+export default client;
