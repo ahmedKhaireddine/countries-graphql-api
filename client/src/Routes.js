7871037023e45969e68a7d248c0a7ff04d55c6e2
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-07-22 20:14:39 +0200

    Improve home route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Countries from './views/Countries';
+import Home from './views/Home';
 import Country from './views/Country';
 
 const routes = [
   {
-    component: Countries,
+    component: Home,
     path: "/"
   },
   {
     component: Country,
-    path: "/:id"
+    path: "/countries/:id"
   }
-]
+];
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        {routes.map(({ component, path }) => {
-            return <Route exact path={path} component={component}/>
+        {routes.map(({ component, path }, index) => {
+            return <Route key={index} exact path={path} component={component}/>
         })}
       </Switch>
     </Router>
