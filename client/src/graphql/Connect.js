3be394400ba13bfb69333864806da6643ff57303
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-07-24 21:09:34 +0200

    Add the error capture of the graphql api

--- client/src/graphql/Connect.js
+++ client/src/graphql/Connect.js
@@ -1,11 +1,29 @@
 import {
     ApolloClient,
-    InMemoryCache
+    InMemoryCache,
+    HttpLink,
+    from
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: 'http://localhost:4000/graphql'
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql"
 });
 
-export default client;
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+    );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const Client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: from([errorLink, httpLink]),
+});
+
+export default Client;
