2dcd88ab57c0c5fe07854f80e14a39cea53fcab4
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2021-08-08 11:42:02 +0200

    Add country query

--- client/src/graphql/Queries.js
+++ client/src/graphql/Queries.js
@@ -3,9 +3,35 @@ import { gql } from "@apollo/client";
 export const GET_COUNTRIES = gql`
     query ($paginate: CountryPaginateInput) {
         countries(paginate: $paginate) {
+            flag
             id
             name
+        }
+    }
+`;
+
+export const GET_COUNTRY = gql`
+    query ($id: ID!) {
+        country(id: $id) {
+            alpha2Code
+            alpha3Code
+            area
+            callingCodes
+            borders
+            capital
+            demonym
             flag
+            id
+            languages
+            latlng
+            nativeName
+            name
+            region
+            population
+            numericCode
+            subregion
+            topLevelDomain
+            translations
         }
     }
 `;
